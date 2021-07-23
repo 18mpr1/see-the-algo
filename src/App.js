66b40e9e1912a2adf713f50f46e74aa2bc6dfb91
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-07-23 01:41:09 -0400

    Initial commit with some basic configuring of elements

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+//import './algorithms/BubbleSort';
+//import './Array.jsx';
+
+function App() {
+  return (
+    <div className="App">
+        <body>
+            <header>See-The-Algo</header>
+            <div id="array">
+
+            </div>
+            <div id="button-bar">
+                <button id="reset">Reset Array</button>
+                <button id="bubble-sort">Bubble Sort</button>
+            </div>
+            <footer>
+                <p><b>Copyright info will go here</b></p>
+            </footer>
+        </body>
+    </div>
+  );
+}
+
+export default App;
