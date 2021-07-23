f7989395ee3732efec1f9d12e130ef9ede1916ee
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-07-23 04:36:46 -0400

    Added more features to do later

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+/*
+<header />
+ <App />
+ <description/>
+ <footer />
+
+ */
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
